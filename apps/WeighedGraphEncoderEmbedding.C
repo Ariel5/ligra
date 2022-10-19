ae2240a17bc4931101aafdf66c897b81c8e6f750
Ariel Lubonja <ariel@cs.jhu.edu>
2022-10-19 12:43:22 -0400

    Reformat WeighedGraphEncoderEmbedding.C

--- apps/WeighedGraphEncoderEmbedding.C
+++ apps/WeighedGraphEncoderEmbedding.C
@@ -34,8 +34,10 @@
 #include "math.h"
 #include <chrono>
 
-void print_to_file(const float* Z, string file_name, const int n, const int k);
+void print_to_file(const float *Z, string file_name, const int n, const int k);
+
 size_t getCurrentRSS();
+
 size_t getPeakRSS();
 
 // TODO Ariel Assuming unweighted graph - No weighed examples in Ligra, despite how supposedly "easy" it is to extend
@@ -64,9 +66,10 @@ struct PR_F { // Do this to edges. But aren't edges defn. by their vertices?
 //            z_curr1(_z_curr1), z_next1(_z_next1), z_curr2(_z_curr2), z_next2(_z_next2), Y(_Y), W(_W), V(_V) {}
 
     float *W;
+
     PR_F(double *_z_curr, float *_z_next, const int _n, int *_Y, float *_W, symmetricVertex *_V)
             : // Constructor. Pass arrays by pointer - easiest way to pass arrays in structs
-    z_curr(_z_curr), z_next(_z_next), n(_n), Y(_Y), W(_W), V(_V) {}
+            z_curr(_z_curr), z_next(_z_next), n(_n), Y(_Y), W(_W), V(_V) {}
 
 
     // s seems to be DESTINATION! d - SOURCE. Found from debugging. TODO may change
@@ -75,10 +78,11 @@ struct PR_F { // Do this to edges. But aren't edges defn. by their vertices?
 
         // Ariel I believe -1 or negative label means don't know - ignored
         if (Y[d] >= 0) { // TODO Ariel TOP I need some kind of += for curr and next
-            z_next[Y[d]*n + s] += W[Y[d]*n + d] * weight; // TODO Ariel Assuming unweighted edges! Ligra has weightedEdge class? Else pass as argument to update()
+            z_next[Y[d] * n + s] += W[Y[d] * n + d] *
+                                    weight; // TODO Ariel Assuming unweighted edges! Ligra has weightedEdge class? Else pass as argument to update()
         }
         if (Y[s] >= 0) {
-            z_next[Y[s]*n + d] += W[Y[s]*n + s] * weight;
+            z_next[Y[s] * n + d] += W[Y[s] * n + s] * weight;
         }
         return 1;
     }
@@ -93,10 +97,9 @@ struct PR_F { // Do this to edges. But aren't edges defn. by their vertices?
 }; // No condition. Apply to all vertices
 
 
-// TODO Ariel Embedding Matrix is kxN - map each vertex to a label
-//  Yet, iterates over edges
+// Embedding Matrix is kxN - map each vertex to a label. GEE iterates over edges
 
-// PRLocalCompute(i) in paper
+// PRLocalCompute(i) in Ligra paper
 //vertex map function to update its p value according to PageRank equation
 struct PR_Vertex_F { // TODO Diff vs. PR_F?
     // TODO Ariel let's reuse p_curr, p_next for self.encoder_embedding
@@ -106,7 +109,7 @@ struct PR_Vertex_F { // TODO Diff vs. PR_F?
     double *p_next;
 
     PR_Vertex_F(double *_p_curr, double *_p_next, double _damping, intE n) : // Bigger constructor? Damping?
-    p_curr(_p_curr), p_next(_p_next),
+            p_curr(_p_curr), p_next(_p_next),
             damping(_damping), addedConstant((1 - _damping) * (1 / (double) n)) {}
 
     inline bool operator()(uintE i) { // TODO Ariel why the index i?
@@ -120,7 +123,7 @@ struct PR_Vertex_Reset {
     double *p_curr;
 
     PR_Vertex_Reset(double *_p_curr) :
-    p_curr(_p_curr) {}
+            p_curr(_p_curr) {}
 
     inline bool operator()(uintE i) {
         p_curr[i] = 0.0;
@@ -130,145 +133,137 @@ struct PR_Vertex_Reset {
 
 
 template<class vertex>
-void Compute(graph<vertex> &GA, commandLine P) { // Call PageRank
-const int k = P.getOptionLongValue("-nClusters", 3); // TODO Ariel Impl. this later
-const string graphName = P.getOptionValue("-graphName", "Facebook");
-const int randomY = P.getOptionIntValue("-randomY", 0);
-//    int k = 3;
+void Compute(graph<vertex> &GA, commandLine P) { // Run GEE
+    const int k = P.getOptionLongValue("-nClusters", 3); // TODO Ariel Impl. this later
+    const string graphName = P.getOptionValue("-graphName", "Facebook");
+    const int randomY = P.getOptionIntValue("-randomY", 0);
 
-const intE n = GA.n;
+    const intE n = GA.n;
 // Run for nr. of edges
-const long maxIters = P.getOptionLongValue("-maxiters", 1);
-const int divideBy2 = P.getOptionLongValue("-divide", 1);
+    const long maxIters = P.getOptionLongValue("-maxiters", 1);
+    const int divideBy2 = P.getOptionLongValue("-divide", 1);
 
-double *p_curr1 = newA(double, 1);
+    double *p_curr1 = newA(double, 1);
 //    { parallel_for (long i = 0; i < n*k; i++) p_curr1[i] = 0; } // Init all in parallel
 //    p_curr1[n*k] = NAN;
-float *p_next1 = newA(float, n*k+1);
-{ parallel_for (long i = 0; i < n*k; i++) p_next1[i] = 0; } //0 if unchanged
-p_next1[n*k] = NAN;
-bool *frontier = newA(bool, n); // Frontier should be whole graph's edges
-{ parallel_for (long i = 0; i < n; i++) frontier[i] = 1; }
+    float *p_next1 = newA(float, n * k + 1);
+    { parallel_for (long i = 0; i < n * k; i++) p_next1[i] = 0; } //0 if unchanged
+    p_next1[n * k] = NAN;
+    bool *frontier = newA(bool, n); // Frontier should be whole graph's edges
+    { parallel_for (long i = 0; i < n; i++) frontier[i] = 1; }
 
-int *Y = newA(int, n); // TODO maybe set some classes to 1. GEE chooses 2 of 5 vertices in class 1
+    int *Y = newA(int, n); // TODO maybe set some classes to 1. GEE chooses 2 of 5 vertices in class 1
 
-if (graphName == "Easy") {
-cout << "Easy graph\n";
-{ parallel_for (long i = 0; i < n; i++) Y[i] = 0; } // Fill with 0-s
-Y[3] = 1;
-Y[4] = 1; // Same as GEE.py easy 5x5 case
-}
-else if (graphName == "Facebook") {
-cout << "Reading Y-facebook-5percent.txt generated in GEE.py case10 semi-supervised";
-string a;
-std::ifstream infile("../../Thesis-Graph-Data/Y-facebook-5percent.txt");
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-int i = 0;
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
+    if (graphName == "Easy") {
+        cout << "Easy graph\n";
+        { parallel_for (long i = 0; i < n; i++) Y[i] = 0; } // Fill with 0-s
+        Y[3] = 1;
+        Y[4] = 1; // Same as GEE.py easy 5x5 case
+    } else if (graphName == "Facebook") {
+        cout << "Reading Y-facebook-5percent.txt generated in GEE.py case10 semi-supervised";
+        string a;
+        std::ifstream infile("../../Thesis-Graph-Data/Y-facebook-5percent.txt");
+        if (infile.fail()) {
+            cout << "\n\nSpecified Y file does not exist\n\n";
+            exit(-1);
+        }
+        int i = 0;
+        if (infile.is_open()) {
+            while (std::getline(infile, a)) {
+                Y[i] = std::stoi(a);
+                i++;
 //                if (i == n) { break; }
-}
-}
-}
-else if (graphName == "LiveJournal") {
-if (randomY == 0) {
-cout << "Reading liveJournal-Y50-sparse generated in GEE.py case10 semi-supervised";
-string a;
-std::ifstream infile("../../Thesis-Graph-Data/liveJournal-Y50-sparse.txt");
-int i = 0;
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
+            }
+        }
+    } else if (graphName == "LiveJournal") {
+        if (randomY == 0) {
+            cout << "Reading liveJournal-Y50-sparse generated in GEE.py case10 semi-supervised";
+            string a;
+            std::ifstream infile("../../Thesis-Graph-Data/liveJournal-Y50-sparse.txt");
+            int i = 0;
+            if (infile.fail()) {
+                cout << "\n\nSpecified Y file does not exist\n\n";
+                exit(-1);
+            }
+            if (infile.is_open()) {
+                while (std::getline(infile, a)) {
+                    Y[i] = std::stoi(a);
+                    i++;
 //                if (i == n) { break; }
-}
-}
-} else {
-cout << "Generating Y at random";
-{ parallel_for (long i = 0; i < n; i++) Y[i] = 3; }
-}
-}
-else if (graphName == "Twitch") {
-cout << "Reading Twitch Y";
-string a;
-std::ifstream infile("../../Thesis-Graph-Data/twitch-Y20-sparse.csv");
-int i = 0;
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
-}
-}
-}
-else if (graphName == "Pokec") {
-cout << "Reading Pokec Y";
-string a;
-std::ifstream infile("../../../Downloads/Thesis-Graph-Data/pokec-Y50-sparse.txt");
-int i = 0;
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
-}
-}
+                }
+            }
+        } else {
+            cout << "Generating Y at random";
+            { parallel_for (long i = 0; i < n; i++) Y[i] = 3; }
+        }
+    } else if (graphName == "Twitch") {
+        cout << "Reading Twitch Y";
+        string a;
+        std::ifstream infile("../../Thesis-Graph-Data/twitch-Y20-sparse.csv");
+        int i = 0;
+        if (infile.fail()) {
+            cout << "\n\nSpecified Y file does not exist\n\n";
+            exit(-1);
+        }
+        if (infile.is_open()) {
+            while (std::getline(infile, a)) {
+                Y[i] = std::stoi(a);
+                i++;
+            }
+        }
+    } else if (graphName == "Pokec") {
+        cout << "Reading Pokec Y";
+        string a;
+        std::ifstream infile("../../../Downloads/Thesis-Graph-Data/pokec-Y50-sparse.txt");
+        int i = 0;
+        if (infile.fail()) {
+            cout << "\n\nSpecified Y file does not exist\n\n";
+            exit(-1);
+        }
+        if (infile.is_open()) {
+            while (std::getline(infile, a)) {
+                Y[i] = std::stoi(a);
+                i++;
+            }
+        }
 //        if chrono::system_clock::now() % 10 <=8 {
 //            Y[i] = -1;
 //        }
 //        Y[i] = std::chrono::system_clock::now() % 50;
-}
-else if (graphName == "Orkut") {
-cout << "Reading Orkut Y. Divide should be 1 for this graph";
-string a;
-std::ifstream infile("../../Thesis-Graph-Data/orkut-Y50-sparse.txt");
-int i = 0;
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
-}
-}
-}
-else if (graphName == "OrkutGroups") {
-cout << "Reading Orkut-Groups Y. Divide should be ? for this graph";
-string a;
-std::ifstream infile("../../../Downloads/Thesis-Graph-Data/orkut-groups-Y40-sparse.txt");
-int i = 0;
-if (infile.fail()) {
-cout << "\n\nSpecified Y file does not exist\n\n";
-exit(-1);
-}
-if (infile.is_open()) {
-while (std::getline(infile, a)) {
-Y[i] = std::stoi(a);
-i++;
-}
-}
-}
-else {
-cout << "Wrong input graph name. Inputs are case sensitive. Possible inputs: Easy, Facebook, LiveJournal\n\n";
-exit(-1);
-}
+    } else if (graphName == "Orkut") {
+        cout << "Reading Orkut Y. Divide should be 1 for this graph";
+        string a;
+        std::ifstream infile("../../Thesis-Graph-Data/orkut-Y50-sparse.txt");
+        int i = 0;
+        if (infile.fail()) {
+            cout << "\n\nSpecified Y file does not exist\n\n";
+            exit(-1);
+        }
+        if (infile.is_open()) {
+            while (std::getline(infile, a)) {
+                Y[i] = std::stoi(a);
+                i++;
+            }
+        }
+    } else if (graphName == "OrkutGroups") {
+        cout << "Reading Orkut-Groups Y. Divide should be ? for this graph";
+        string a;
+        std::ifstream infile("../../../Downloads/Thesis-Graph-Data/orkut-groups-Y40-sparse.txt");
+        int i = 0;
+        if (infile.fail()) {
+            cout << "\n\nSpecified Y file does not exist\n\n";
+            exit(-1);
+        }
+        if (infile.is_open()) {
+            while (std::getline(infile, a)) {
+                Y[i] = std::stoi(a);
+                i++;
+            }
+        }
+    } else {
+        cout << "Wrong input graph name. Inputs are case sensitive. Possible inputs: Easy, Facebook, LiveJournal\n\n";
+        exit(-1);
+    }
 
 //    cout <<
 //#nk: 1*n array, contains the number of observations in each class
@@ -276,41 +271,41 @@ exit(-1);
 
 // Not doing possibility_detected
 //    int nk[2] = {3,2};
-int nk[k]; // Confirmed correct Facebook graph
+    int nk[k]; // Confirmed correct Facebook graph
 // TODO Ariel implement count_nonzero later. Should return nk = {3,2}
-for (int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
 // TODO Ariel Why need count of indices nk?
-int nonzeroYCount = 0;
-for (int j = 0; j < n; j++) {// nk = np.count_nonzero(Y[:,0]==i)
-if (Y[j] == i)
-nonzeroYCount++;
-}
-nk[i] = nonzeroYCount;
-}
+        int nonzeroYCount = 0;
+        for (int j = 0; j < n; j++) {// nk = np.count_nonzero(Y[:,0]==i)
+            if (Y[j] == i)
+                nonzeroYCount++;
+        }
+        nk[i] = nonzeroYCount;
+    }
 
-vertexSubset Frontier(n, n, frontier); // TODO TOP What does this do?
+    vertexSubset Frontier(n, n, frontier); // TODO TOP What does this do?
 
-float *W = newA(float, n*k+1); // W seems ok too, not confirmed tho
-{ parallel_for (long i = 0; i < n*k; i++) W[i] = 0; }
-W[n*k] = NAN;
+    float *W = newA(float, n * k + 1); // W seems ok too, not confirmed tho
+    { parallel_for (long i = 0; i < n * k; i++) W[i] = 0; }
+    W[n * k] = NAN;
 
-for (int i = 0; i < n; i++) { // For i in range(Y.shape[0])
-int k_i = Y[i]; // TODO LOW Using 1D Y
-if (k_i >= 0)
-W[k_i*n + i] = 1.0 / nk[k_i];
-}
+    for (int i = 0; i < n; i++) { // For i in range(Y.shape[0])
+        int k_i = Y[i]; // TODO LOW Using 1D Y
+        if (k_i >= 0)
+            W[k_i * n + i] = 1.0 / nk[k_i];
+    }
 // So far, W is good
 
 // TODO TOP Each vertex has (kxn) Z-matrix?
-long iter = 0;
-while (iter++ < maxIters) {
-edgeMap(GA, Frontier, PR_F<vertex>(p_curr1, p_next1, n, Y, W, GA.V), 0, no_output);
+    long iter = 0;
+    while (iter++ < maxIters) {
+        edgeMap(GA, Frontier, PR_F<vertex>(p_curr1, p_next1, n, Y, W, GA.V), 0, no_output);
 
-if (divideBy2 != 0) {
-{
-parallel_for (long i = 0; i < n * k; i++) p_next1[i] /= 2;
-} // TODO lol fix this. Ligra assumes undirected? goes over all edges twice
-}
+        if (divideBy2 != 0) {
+            {
+                parallel_for (long i = 0; i < n * k; i++) p_next1[i] /= 2;
+            } // TODO lol fix this. Ligra assumes undirected? goes over all edges twice
+        }
 
 //        cout << "\niter: " << iter << "\n\n";
 
@@ -324,7 +319,7 @@ parallel_for (long i = 0; i < n * k; i++) p_next1[i] /= 2;
 //        vertexMap(Frontier, PR_Vertex_Reset(p_curr2));
 //        swap(p_curr1, p_next1);
 //        swap(p_curr2, p_next2);
-}
+    }
 //    cout << "Current Embedding values (Z-projection): " << *p_curr;
 //    cout << "W: "<<W;
 
@@ -337,21 +332,21 @@ parallel_for (long i = 0; i < n * k; i++) p_next1[i] /= 2;
 //    cout << "\n\n\n--------------Finished one whole run----------\n\n\n";
 
 // Use this to print output to file to test correctness
-print_to_file(p_next1, "./testing/outputs_to_compare/Ligra_outputs/Z_output.csv", n, k);
+    print_to_file(p_next1, "./testing/outputs_to_compare/Ligra_outputs/Z_output.csv", n, k);
 
 // Use this to check RAM usage
 //    cout << "current Residual Set Size (RAM usage): " << (float) getCurrentRSS() / (1024*1024) << " MB\n\n";
 //    cout << "Peak Residual Set Size (RAM usage): " << (float) getPeakRSS() / (1024*1024) << " MB\n\n";
 
-Frontier.del();
-free(p_curr1);
-free(p_next1);
-free(W);
-free(Y);
+    Frontier.del();
+    free(p_curr1);
+    free(p_next1);
+    free(W);
+    free(Y);
 }
 
 // n - nr. vertices. k - nr. classes
-void print_to_file(const float* Z, string file_name, const int n, const int k) {
+void print_to_file(const float *Z, string file_name, const int n, const int k) {
     cout << "\n\nSaving Z to " << file_name << "\n";
     std::ofstream outfile(file_name);
     if (outfile.is_open()) {
@@ -359,9 +354,9 @@ void print_to_file(const float* Z, string file_name, const int n, const int k) {
         while (i < n) {
             string row = "";
 //            if (i % kn == 0)
-            for (int j=0; j<k; j++) {
-                row.append(std::to_string(Z[j*n + i]));
-                if (j != k-1) {
+            for (int j = 0; j < k; j++) {
+                row.append(std::to_string(Z[j * n + i]));
+                if (j != k - 1) {
                     row.append(" "); // Space separated values
                 }
             }
@@ -384,10 +379,12 @@ void print_to_file(const float* Z, string file_name, const int n, const int k) {
 #include <psapi.h>
 
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+
 #include <unistd.h>
 #include <sys/resource.h>
 
 #if defined(__APPLE__) && defined(__MACH__)
+
 #include <mach/mach.h>
 
 #elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
@@ -404,16 +401,12 @@ void print_to_file(const float* Z, string file_name, const int n, const int k) {
 #endif
 
 
-
-
-
 /**
  * Returns the peak (maximum so far) resident set size (physical
  * memory use) measured in bytes, or zero if the value cannot be
  * determined on this OS.
  */
-size_t getPeakRSS( )
-{
+size_t getPeakRSS() {
 #if defined(_WIN32)
     /* Windows -------------------------------------------------- */
     PROCESS_MEMORY_COUNTERS info;
@@ -437,9 +430,9 @@ size_t getPeakRSS( )
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
     /* BSD, Linux, and OSX -------------------------------------- */
     struct rusage rusage;
-    getrusage( RUSAGE_SELF, &rusage );
+    getrusage(RUSAGE_SELF, &rusage);
 #if defined(__APPLE__) && defined(__MACH__)
-    return (size_t)rusage.ru_maxrss;
+    return (size_t) rusage.ru_maxrss;
 #else
     return (size_t)(rusage.ru_maxrss * 1024L);
 #endif
@@ -451,15 +444,11 @@ size_t getPeakRSS( )
 }
 
 
-
-
-
 /**
  * Returns the current resident set size (physical memory use) measured
  * in bytes, or zero if the value cannot be determined on this OS.
  */
-size_t getCurrentRSS( )
-{
+size_t getCurrentRSS() {
 #if defined(_WIN32)
     /* Windows -------------------------------------------------- */
     PROCESS_MEMORY_COUNTERS info;
@@ -470,10 +459,10 @@ size_t getCurrentRSS( )
     /* OSX ------------------------------------------------------ */
     struct mach_task_basic_info info;
     mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
-    if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
-        (task_info_t)&info, &infoCount ) != KERN_SUCCESS )
-        return (size_t)0L;      /* Can't access? */
-    return (size_t)info.resident_size;
+    if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO,
+                  (task_info_t) &info, &infoCount) != KERN_SUCCESS)
+        return (size_t) 0L;      /* Can't access? */
+    return (size_t) info.resident_size;
 
 #elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
     /* Linux ---------------------------------------------------- */
