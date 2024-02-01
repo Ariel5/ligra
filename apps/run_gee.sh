17d6b2b1041fe0abc7a505017b74e908ce75be77
Ariel Lubonja <ariel@cs.jhu.edu>
2024-02-01 13:57:09 -0500

    Add script to automate running Erdos graphs

---
+++ apps/run_gee.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the directories
+ys_dir="/home/ubuntu/prog/erdos-renyi-10-degree/Ys"
+ligra_dir="/home/ubuntu/prog/erdos-renyi-10-degree/Ligra-AdjGraphs"
+
+# Loop through the Ys directory
+for ys_file in "$ys_dir"/*.csv; do
+  # Extract the base name without the extension
+  base_name=$(basename "$ys_file" .csv)
+
+  # Construct the path to the corresponding Ligra file
+  ligra_file="$ligra_dir/${base_name}.AdjGraph"
+
+  # Run your command with the Ys file and the Ligra file
+  ./GraphEncoderEmbedding -rounds 7 -nClusters 50 -saveEmbedding false -Laplacian false -yLocation "$ys_file" "$ligra_file"
+done
